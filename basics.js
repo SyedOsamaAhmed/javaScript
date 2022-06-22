b1f96f3bdcc708f5f9700005f3a17f81eb8c9070
Osama <batch17.026@gmail.com>
2022-06-22 13:48:28 +0500

    learned variablesand rules along with history

---
+++ basics.js
@@ -0,0 +1,20 @@
+//Variables: for stoeing and using data for later:
+"use strict"
+var firstName = 'Osama';
+
+//console.log(firstName);
+
+firstName='Rabia';
+// console.log(firstName);
+/*Rules for naming variables:
+1- variable name cannot start with number.
+2- only _ or $ is allowed in naming variables.
+2.1- only first_name(Snakecase) or _firstName are valid and same in $ case.
+3- no spaces allowed in variable
+4- variables should be camelcase i.e highAltitude */
+
+var value= 10;
+// console.log(value ** 2);  //power 
+// console.log(value * 2);
+// console.log(value / 5);
+console.log(value ** 0.5);
