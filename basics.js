40e9d0a71d66b842b8c82cbe38faef28e3e151c2
Osama <batch17.026@gmail.com>
2022-06-23 10:01:04 +0500

    learned basic string and their methods plus conversion

--- basics.js
+++ basics.js
@@ -1,10 +1,10 @@
 //Variables: for stoeing and using data for later:
-"use strict"
-var firstName = 'Osama';
+"use strict";
+var firstName = "Osama";
 
 //console.log(firstName);
 
-firstName='Rabia';
+firstName = "Rabia";
 // console.log(firstName);
 /*Rules for naming variables:
 1- variable name cannot start with number.
@@ -13,8 +13,30 @@ firstName='Rabia';
 3- no spaces allowed in variable
 4- variables should be camelcase i.e highAltitude */
 
-var value= 10;
-// console.log(value ** 2);  //power 
+var value = 10;
+// console.log(value ** 2);  //power
 // console.log(value * 2);
 // console.log(value / 5);
-console.log(value ** 0.5);
+// console.log(value ** 0.5);
+
+//Strings:(immutable)
+let name_ = "OSAMA";
+// console.log(name_.trim());//trim() method remove spaces in string
+// console.log(name_.toUpperCase());
+// console.log(name_.toLocaleLowerCase());
+
+// console.log(name_.slice(1,4));
+
+let age = 23,
+  first$name = "Isma";
+// console.log(typeof age);
+// console.log(typeof first$name);
+
+//Number to strings:
+// console.log(typeof (age + ""));
+// console.log(typeof String(age))
+
+//string to Number:
+let str = +"34";
+//console.log(typeof str);
+console.log(typeof Number(str));
